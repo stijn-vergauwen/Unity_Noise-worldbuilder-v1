b8eff8ab249d9d3b06222fe870d69bc938006aa3
Stijn Vergauwen <85249104+stijn301@users.noreply.github.com>
2022-12-27 12:12:35 +0100

    add plant destroy effect

--- Assets/_Scripts/World/PickablePlant.cs
+++ Assets/_Scripts/World/PickablePlant.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PickablePlant : MonoBehaviour, IInteractable
 {
+  [SerializeField] GameObject destroyEffect;
+
   public void Activate() {
+    Destroy(Instantiate(destroyEffect, transform.position, Quaternion.identity), 10);
     Destroy(gameObject);
   }
 
